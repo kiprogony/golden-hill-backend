0d254c3f2b1dbea6b2a2a90d4c2bdc3b05e4b7d2
Daryl Rogony <darylrogony@gmail.com>
2022-07-31 12:56:58 +0300

    FIt: added an attribute to managers table

--- db/migrate/20220731095412_create_managers.rb
+++ db/migrate/20220731095412_create_managers.rb
@@ -4,7 +4,8 @@ class CreateManagers < ActiveRecord::Migration[6.1]
       t.string :name
       t.integer :mobile_no
       t.integer :id_no
-
+      t.string :department
+      
       t.timestamps
     end
   end
