fe1e084fea2652ab8dba1024b208f8d5a9c2e828
Daryl Rogony <darylrogony@gmail.com>
2022-08-04 20:52:04 +0300

    Fix: association

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,8 @@
 class Employee < ApplicationRecord
   validates :name, presence: true
-  # validates :email, uniqueness: true
+  validates :email, uniqueness: true
   validates :mobile_no, length: { in: 0..9 }
+
   belongs_to :user
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password  
   validates :username, presence: true
+  validates :username, uniqueness: true
+
   has_many :employees
 end
