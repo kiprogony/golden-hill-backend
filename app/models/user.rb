027b242d13fd64c0adbc7a2b12e001aee1bbd746
Daryl Rogony <darylrogony@gmail.com>
2022-08-05 00:13:34 +0300

    Fit: add authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_secure_password  
   validates :username, presence: true
   validates :username, uniqueness: true
+  validates :email, presence: true
 
   has_many :employees
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later 
   resources :employees
-  resources :users, only: [:index, :show]
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"  
+  delete "/logout", to: "sessions#destroy"        
   
 
   # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
